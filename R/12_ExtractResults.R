38900d6470a1c2cded9b9c23a3dcf0faf3230e74
Konstantinoudis <gkonstan@ic.ac.uk>
2025-05-24 11:56:44 +0100

    Add the plots and tables

--- R/12_ExtractResults.R
+++ R/12_ExtractResults.R
@@ -9,6 +9,7 @@
 library(tidyverse)
 library(INLA)
 library(patchwork)
+library(xtable)
 
 
 # set path
@@ -149,6 +150,10 @@ ggplot(data =
 
 (((p1)|(p3)) + plot_layout(widths = c(1, 2)))/
   ((p2|p4) + plot_layout(widths = c(1, 2))) 
+
+
+ggsave(filename = "output/fig1.png", dpi = 300, width = 8)
+
 # +
 #   plot_annotation('Observed and expected mortality', 
 #                   theme=theme(plot.title=element_text(hjust=0.5)))
@@ -181,7 +186,7 @@ ggplot(data =
 # absolute across the country
 
 res %>% 
-  dplyr::select(date, NUTSII, age, sex) %>% 
+  dplyr::select(date, NUTSII, age, sex, true_values) %>% 
   cbind(., 
         sweep(
           x = res %>% 
@@ -192,7 +197,7 @@ res %>%
         )*(-1)) -> excess
 
 
-getSums2 <- function(X, Y = NULL){
+getSums2 <- function(X, Y = NULL, groupd = FALSE){
   
   if(sum(X %in% "date") == 1){
     if(is.null(Y)){
@@ -200,6 +205,11 @@ getSums2 <- function(X, Y = NULL){
     }
     excess %>% 
       dplyr::filter(date %in% Y) -> excess.fun
+    
+    if(groupd == TRUE){
+      X <- X[!(X %in% "date")]
+    }
+    
   }else{
     excess.fun <- excess
   }
@@ -217,9 +227,148 @@ getSums2 <- function(X, Y = NULL){
   
   return(df_plot)
 }
-getSums2(X = c("date", "NUTSII", "age", "sex"), Y = "2025-04-28") %>% View()
-getSums2(c("date", "NUTSII", "sex"), Y = "2025-04-28")
-getSums2(c("date"), Y = c("2025-04-28", "2025-04-29", "2025-04-30"))
-getSums2(X = c("date", "NUTSII"), Y = c("2025-04-28", "2025-04-29", "2025-04-30")) %>% View()
 
-getSums2(c("date"))
+# getSums2(X = c("date", "NUTSII", "age", "sex"), Y = "2025-04-28") %>% View()
+# getSums2(c("date", "NUTSII", "sex"), Y = "2025-04-28")
+# getSums2(c("date"), Y = c("2025-04-28", "2025-04-29", "2025-04-30"))
+# getSums2(X = c("date", "NUTSII"), Y = c("2025-04-28", "2025-04-29", "2025-04-30")) %>% View()
+# getSums2(X = c("date","age", "NUTSII"), Y = c("2025-04-28", "2025-04-29", "2025-04-30")) %>% View()
+
+
+# 2025-04-28
+getTable <- function(Z, groupd = FALSE){
+  rbind(
+    getSums2(c("date", "age", "sex"), Y = c(Z), groupd = groupd) %>% 
+      mutate(
+        CrI = paste0(round(`50%`), " (", round(`2.5%`), ", ", round(`97.5%`), ")")) %>% 
+      dplyr::select(!c(`50%`, `2.5%`, `97.5%`)), 
+    
+    getSums2(c("date", "age"), Y = c(Z), groupd = groupd) %>%
+      mutate(sex = "Total", 
+             CrI = paste0(round(`50%`), " (", round(`2.5%`), ", ", round(`97.5%`), ")")) %>% 
+      dplyr::select(!c(`50%`, `2.5%`, `97.5%`)),
+    
+    getSums2(c("date", "sex"), Y = c(Z), groupd = groupd) %>% 
+      mutate(sex2=sex, sex=NULL) %>% 
+      mutate(age = "Total", 
+             sex = sex2,
+             sex2=NULL,
+             CrI = paste0(round(`50%`), " (", round(`2.5%`), ", ", round(`97.5%`), ")")) %>% 
+      dplyr::select(!c(`50%`, `2.5%`, `97.5%`)),
+    
+    
+    getSums2(c("date"), Y = c(Z), groupd = groupd) %>%
+      mutate(age = "Total", sex = "Total", 
+             CrI = paste0(round(`50%`), " (", round(`2.5%`), ", ", round(`97.5%`), ")")) %>% 
+      dplyr::select(!c(`50%`, `2.5%`, `97.5%`)) %>% return()
+  )
+}
+
+purrr::reduce(lapply(c("2025-04-28", "2025-04-29", "2025-04-30"), getTable), 
+              dplyr::left_join, by = c("age", "sex")) -> tab1
+
+tab1$date.x <- tab1$date.y <- tab1$date <- NULL
+colnames(tab1)[-c(1:2)] <- c("2025-04-28", "2025-04-29", "2025-04-30")
+
+print(xtable(tab1), include.rownames=FALSE)
+
+
+##
+## Cumulative table
+purrr::reduce(lapply(list(
+  c("2025-04-28"),
+  c("2025-04-28", "2025-04-29", "2025-04-30"),
+  c("2025-04-28", "2025-04-29", "2025-04-30", "2025-05-01", "2025-05-02", "2025-05-03", "2025-05-04")
+), getTable, groupd = TRUE), dplyr::left_join, by = c("age", "sex")) -> tab1
+
+tab1$age[tab1$age %in% "0-64"] <- "65<"
+tab1$age[tab1$age %in% "85+"] <- ">84"
+
+tab1$sex[tab1$sex %in% "female"] <- "Females"
+tab1$sex[tab1$sex %in% "male"] <- "Males"
+tab1$sex <- factor(tab1$sex, levels = c("Males", "Females", "Total"))
+tab1$age <- factor(tab1$age, levels = c("65<", "65-84", ">84", "Total"))
+
+tab1 <- tab1 %>% arrange(age, sex)
+colnames(tab1)[-c(1:2)] <- c("Blackout", "2 days after", "1 week after")
+print(xtable(tab1), include.rownames=FALSE)
+
+
+
+
+##
+## and something about space, like relative excess. 
+getRelativeExcess <- function(X, Y = NULL, groupd = FALSE){
+    
+    if(sum(X %in% "date") == 1){
+      if(is.null(Y)){
+        Y <- unique(excess$date)
+      }
+      excess %>% 
+        dplyr::filter(date %in% Y) -> excess.fun
+      
+      if(groupd == TRUE){
+        X <- X[!(X %in% "date")]
+      }
+      
+    }else{
+      excess.fun <- excess
+    }
+  
+
+    
+    excess.fun %>% 
+      dplyr::group_by_at(X) %>% 
+      summarise(across(c(paste0("V", 1:200)), list(sum))) -> res_tot 
+    
+    res %>% 
+      dplyr::select(date, NUTSII, age, sex, true_values, starts_with("V")) %>% 
+      dplyr::group_by_at(X) %>% 
+      summarise(across(c(paste0("V", 1:200)), list(sum))) -> res_exp
+    
+    (res_tot %>% ungroup() %>% dplyr::select(starts_with("V")))/
+      (res_exp %>% ungroup() %>% dplyr::select(starts_with("V"))) -> relativeex
+    
+    # cbind(excess.fun %>% dplyr::select(all_of(X)), relativeex)
+    
+    test <- relativeex[,paste0("V", 1:200, "_1")] %>% 
+      apply(., 1, function(x) quantile(x, 
+                                       probs = c(0.5, 0.025, 0.975)))
+    
+    df_plot <- test %>% t() %>% as.data.frame()
+    df_plot <- cbind(res_tot %>% dplyr::select(-(paste0("V", 1:200, "_1"))), df_plot)
+    
+    return(df_plot)
+}
+
+
+(getRelativeExcess(c("date", "NUTSII"), 
+                   groupd = TRUE, 
+                   Y = c("2025-04-28")) %>% 
+    ggplot() + 
+    geom_hline(yintercept=0, col = "red", lty = 2) + 
+    geom_point(aes(x=NUTSII, y = `50%`)) + 
+    geom_errorbar(aes(x=NUTSII, ymin = `2.5%`, ymax = `97.5%`), width = 0.2)) +
+  theme_bw() + ylab("") + ggtitle("A. Blackout day") + 
+  xlab("") -> p1
+
+
+(getRelativeExcess(c("date", "NUTSII"), 
+                     groupd = TRUE, 
+                     Y = c("2025-04-28", "2025-04-29", "2025-04-30")) %>% 
+     ggplot() + 
+     geom_hline(yintercept=0, col = "red", lty = 2) + 
+     geom_point(aes(x=NUTSII, y = `50%`)) + 
+     geom_errorbar(aes(x=NUTSII, ymin = `2.5%`, ymax = `97.5%`), width = 0.2)) + 
+  theme_bw() + ylab("") +  xlab("") + ggtitle("B. Cummulative effect 2 days after") -> p2
+
+(getRelativeExcess(c("NUTSII")) %>% 
+     ggplot() + 
+     geom_hline(yintercept=0, col = "red", lty = 2) + 
+     geom_point(aes(x=NUTSII, y = `50%`)) + 
+     geom_errorbar(aes(x=NUTSII, ymin = `2.5%`, ymax = `97.5%`), width = 0.2)) + 
+  theme_bw() + ylab("") + ggtitle("C. Cummulative effect 1 week after") -> p3
+
+p1/p2/p3
+
+ggsave(filename = "output/fig2.png", dpi = 300, width = 8)
